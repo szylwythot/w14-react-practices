7329757dc1469d66f57d1c90d367412058775b6d
toth_szilvia <szylwy@gmail.com>
2022-04-20 17:19:18 +0200

    use react method

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 function Header() {
-  return <div>Header</div>;
+    // return <div>Header</div>;
+    return React.createElement("div", null, `Header`);
 }
 
 export default Header;
