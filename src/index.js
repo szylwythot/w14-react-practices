5370a058fc242ed1aa759f087311017eaa058f9b
toth_szilvia <szylwy@gmail.com>
2022-04-20 17:08:28 +0200

    initial header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import ButtonWithClass from './component/ButtonWithClass';
 import ButtonWithHooks from './component/ButtonWithHook';
+import Header from './component/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <ButtonWithClass />
     <ButtonWithHooks />
   </React.StrictMode>
